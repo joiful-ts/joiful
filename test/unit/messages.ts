afec8e208c1393f69b08f881031477d854fb6bc4
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2016-05-08 13:32:56 +1000

    Add a test for messages.

---
+++ test/unit/messages.ts
@@ -0,0 +1,27 @@
+import * as chai from "chai";
+import AssertStatic = Chai.AssertStatic;
+var assert : AssertStatic = chai.assert;
+import {Validator} from "../../src/Validator";
+import {Length} from "../../src/constraints/string/Length";
+import {isValid} from "./testUtil";
+import {isInvalid} from "./testUtil";
+
+describe('Messages', function () {
+    it('default message', function () {
+        let validator = new Validator();
+
+        class ClassToValidate {
+            @Length(5)
+            public myProperty : string;
+        }
+
+        let instance = new ClassToValidate();
+        instance.myProperty = "abc";
+
+        let result = validator.validate(instance);
+        assert.property(result, "error");
+        assert.isNotNull(result.error, "Validation should fail");
+        assert.equal(result.error.details[0].message, `"myProperty" length must be 5 characters long`);
+    });
+
+});
