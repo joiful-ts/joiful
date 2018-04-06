a836e6bfd6dda84cf1c40f074df44c127e2d9471
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2018-04-07 09:57:41 +1000

    Tweaks to the build script: Parse package.json as JSON. Use variables to store the root and output directories. Use "dirname" instead of ".." to deterine the root directory.

--- src/build.ts
+++ src/build.ts
@@ -1,25 +1,34 @@
 import {readFileSync, writeFileSync} from "fs";
-import {join} from "path";
+import {dirname, join, sep} from "path";
 
 function copyFile(inPath : string, outPath : string) {
     writeFileSync(outPath, readFileSync(inPath));
 }
 
+interface PackageJson {
+    private: boolean;
+    main: string;
+    types: string;
+}
+
 function build() {
+    const rootDir = dirname(__dirname);
+    const outputDir = __dirname;
+
     // Read package.json
-    let packageContents = readFileSync(join(__dirname, `..`, `package.json`), 'utf8');
-    packageContents = packageContents
-        .replace(/dist\//g, '') // Replace "dist/" with "". (Not foolproof, but good enough)
-        .replace(/"private": true/, `"private": false`); // Make this publishable
+    let packageContents : PackageJson = JSON.parse(readFileSync(join(rootDir, `package.json`), 'utf8'));
+    packageContents.private = false;
+    packageContents.main = packageContents.main.replace(/dist\//g, '');
+    packageContents.types = packageContents.types.replace(/dist\//g, '');
     // Write it out to the publishing directory
-    writeFileSync(join( __dirname , `package.json`), packageContents);
+    writeFileSync(join( outputDir , `package.json`), JSON.stringify(packageContents, null, 2)); // NOTE: this file will have LF line endings
     // Copy some other files to publish into the publishing directory
     const filesToCopy = [
         '.npmignore',
         'README.md'
     ];
     for (const fileToCopy of filesToCopy) {
-        copyFile(join(__dirname, `..`, fileToCopy), join(__dirname, fileToCopy));
+        copyFile(join(rootDir, fileToCopy), join(outputDir, fileToCopy));
     }
 }
 
