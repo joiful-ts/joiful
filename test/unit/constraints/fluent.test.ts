cb746bba6d4545fe2eb22278a9b382912953912b
Ben <codeandcats@gmail.com>
2019-05-12 20:13:27 +1000

    fix(test): rename fluent.ts to fluent.test.ts

---
+++ test/unit/constraints/fluent.test.ts
@@ -0,0 +1,43 @@
+import "../metadataShim";
+import { registerJoi } from "../../../src/core";
+import * as joi from "joi";
+import { JoiSchema } from "../../../src/constraints/fluent";
+import { testConstraintWithPojos } from '../testUtil';
+
+registerJoi(joi);
+
+describe('Fluent API', () => {
+  class SignUpForm {
+    @JoiSchema(joi => joi.string().email().required())
+    emailAddress: string = '';
+
+    @JoiSchema(joi => joi.string().min(8).required())
+    password: string = '';
+
+    @JoiSchema(joi => joi.string().required())
+    firstName: string = '';
+
+    @JoiSchema(joi => joi.string().required())
+    lastName: string = '';
+
+    @JoiSchema(joi => joi.number())
+    age?: number;
+  }
+
+  testConstraintWithPojos(
+    () => SignUpForm,
+    [{
+      emailAddress: 'valid@example.com',
+      password: 'swordfish',
+      firstName: 'Hugh',
+      lastName: 'Hackman',
+      age: 33
+    }],
+    [{
+      emailAddress: 'invalid',
+      password: '',
+      firstName: 'Hugh',
+      lastName: 'Hackman',
+    }]
+  );
+});
