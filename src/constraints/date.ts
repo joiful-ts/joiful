dade4061f0ad8d74e97b510f1d7fd495a347d49b
Ben <codeandcats@gmail.com>
2019-07-18 21:53:50 +1000

    docs(dates): add javadoc comments for date constraints

--- src/constraints/date.ts
+++ src/constraints/date.ts
@@ -3,6 +3,10 @@ import { constraintDecorator, typeConstraintDecorator, TypedPropertyDecorator } 
 
 type AllowedPropertyTypes = Date | string;
 
+/**
+ * Forces the property to be a Date object.
+ * @param schemaBuilder Optionally allows you to chain together additional constraints.
+ */
 export function DateSchema(
     schemaBuilder?: (schema: DateSchema) => DateSchema,
 ): TypedPropertyDecorator<AllowedPropertyTypes> {
@@ -15,24 +19,39 @@ export function DateSchema(
     });
 }
 
+/**
+ * Coerces value to a Date object from string value in valid ISO 8601 date format.
+ */
 export function Iso(): TypedPropertyDecorator<AllowedPropertyTypes> {
     return constraintDecorator<AllowedPropertyTypes>((schema: Schema) => {
         return (schema as DateSchema).iso();
     });
 }
 
+/**
+ * Specifies the maximum value.
+ * @param limit The maximum value.
+ */
 export function Max(limit: number | 'now' | string | Date | Reference): TypedPropertyDecorator<AllowedPropertyTypes> {
     return constraintDecorator<AllowedPropertyTypes>((schema: Schema) => {
         return (schema as DateSchema).max(<any>limit);
     });
 }
 
+/**
+ * Specifies the minimum value.
+ * @param limit The minimum value.
+ */
 export function Min(limit: number | 'now' | string | Date | Reference): TypedPropertyDecorator<AllowedPropertyTypes> {
     return constraintDecorator<AllowedPropertyTypes>((schema: Schema) => {
         return (schema as DateSchema).min(<any>limit);
     });
 }
 
+/**
+ * Coerces value to a Date object from a timestamp interval from Unix/Javascript Time
+ * @param type unix or javaScript
+ */
 export function Timestamp(type?: 'unix' | 'javascript'): TypedPropertyDecorator<AllowedPropertyTypes> {
     return constraintDecorator<AllowedPropertyTypes>((schema: Schema) => {
         return (schema as DateSchema).timestamp(type);
